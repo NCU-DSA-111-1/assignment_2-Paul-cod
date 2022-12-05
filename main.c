04ef622e9b0fe9a94895a9ee59923b8dd11b9713
Paul <paul3092v@gmail.com>
2022-12-05 09:52:44 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <stdio.h>
+//int argc , *argv[]
+int main() {
+
+    char input;
+    printf("enter s to save the game：");
+    scanf("%c", &input);
+    int a=(int)input;
+    printf("%d",a);
+    return 0;
+
+}
+
